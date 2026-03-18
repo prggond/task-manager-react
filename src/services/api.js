f4f042367554e815965436603cc19ba8ed7275b0
Praveen gond <p50153168@gmail.com>
2026-03-18 13:07:50 +0530

    fixed api url

--- src/services/api.js
+++ src/services/api.js
@@ -29,7 +29,8 @@
 // export default api;
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+// const API_URL = 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
